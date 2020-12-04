26a6f9df746ce30395ae75919b0863040b6d3186
Crew Spence <csspence123@gmail.com>
2020-12-04 05:58:18 -0600

    Wrote working solution.

--- codewars_parse_float.js
+++ codewars_parse_float.js
@@ -2,6 +2,12 @@
 Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
 */
 
-function parseF(s) {
+const parseF = (s) => {
+  if(typeof s === 'boolean' || s === undefined) {
+    return null;
+  }
+  if(Number(s) * 0 === 0) {
+    return Number(s);
+  }
   return null;
 }
